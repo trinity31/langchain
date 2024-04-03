68c944d6a1dfd8b4aebb4d86f9a1d27096402319
Kyeongyeon Song <kyeongyeon.song@gmail.com>
2024-04-03 12:54:43 +0900

    DocumentGPT

---
+++ Home.py
@@ -0,0 +1,23 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="FullstackGPT Home",
+    page_icon="🤖",
+)
+
+st.markdown(
+    """
+# Hello!
+            
+Welcome to my FullstackGPT Portfolio!
+            
+Here are the apps I made:
+            
+- [x] [📃 DocumentGPT](/DocumentGPT)
+- [] [🔒 PrivateGPT](/PrivateGPT)
+- [] [❓ QuizGPT](/QuizGPT)
+- [] [🖥️ SiteGPT](/SiteGPT)
+- [] [💼 MeetingGPT](/MeetingGPT)
+- [] [📈 InvestorGPT](/InvestorGPT)
+"""
+)
